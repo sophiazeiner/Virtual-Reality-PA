ead1e2784dc52aee5d4c36ce1213d9d1ffd00fa6
isalein2001 <prieb21868@hs-ansbach.de>
2025-07-06 10:31:41 +0200

    Handschuhe als greifbares Objekt
    - XR Grab Interactable hinzugefügt und auf "HANDSCHUHE" angewendet
    - Einstellungen verfeinert, da es am Anfang nicht funktioniert hat
    - Rigidbody hinzugefügt
    - Code geskriptet, damit die Handschuhe verschwinden, sobald man sie angeklickt hat (als hätte man sie angezogen)
    - Code geskriptet, damit die Handschuhe kurz aufleuchten, wenn man mit dem Controller drübergeht

---
+++ UnityProject/Assets/Isa/Skript/HoverHighlights_Handschuhe.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HoverHighlights_Handschuhe : MonoBehaviour
+{
+    public Material highlightMaterial;
+    public Material defaultMaterial;
+
+    private MeshRenderer[] meshRenderers;
+    private XRGrabInteractable grabInteractable;
+
+    private void Start()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        if (grabInteractable != null)
+        {
+            grabInteractable.hoverEntered.AddListener(OnHoverEnter);
+            grabInteractable.hoverExited.AddListener(OnHoverExit);
+        }
+        else
+        {
+            Debug.LogWarning("XRGrabInteractable missing on: " + gameObject.name);
+        }
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.material = highlightMaterial;
+        }
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args)
+    {
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.material = defaultMaterial;
+        }
+    }
+}
+
